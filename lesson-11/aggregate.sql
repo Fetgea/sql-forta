2f954f0b85b4edbb396b21804c0ff0cf06b87ac9
Dinar <dinar@reaspekt.com>
2021-05-19 10:27:11 +0300

    changed subquery formatting

--- lesson-11/aggregate.sql
+++ lesson-11/aggregate.sql
@@ -2,9 +2,10 @@ SELECT COUNT(*) AS Orders
 FROM Orders
 WHERE cust_id = 1;
 
-SELECT cust_name, cust_state, (SELECT COUNT(*)
-                               FROM Orders
-                               WHERE Orders.cust_id = Customers.cust_id) AS orders
+SELECT cust_name, cust_state, 
+       (SELECT COUNT(*)
+        FROM Orders
+        WHERE Orders.cust_id = Customers.cust_id) AS orders
 FROM Customers
 ORDER BY cust_name;
 
@@ -12,8 +13,9 @@ ORDER BY cust_name;
 Пример запроса возвращающего неправильные данные
 */
 
-SELECT cust_name, cust_state, (SELECT COUNT(*)
-                               FROM Orders
-                               WHERE cust_id = cust_id) AS orders
+SELECT cust_name, cust_state, 
+       (SELECT COUNT(*)
+        FROM Orders
+        WHERE cust_id = cust_id) AS orders
 FROM Customers
 ORDER BY cust_name;
